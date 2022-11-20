0030f00958e3fcc8eb88b990d994dad15fbfa2e2
GabrielDSousa <gabrielramos.email@gmail.com>
2022-11-20 14:12:59 -0300

    One Vue Component Per File

---
+++ js/components/AppButton.js
@@ -0,0 +1,12 @@
+export default {
+    template:`
+        <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded disabled:cursor-not-allowed" :disabled="processing">
+            <slot></slot>
+        </button>
+    `,
+    data() {
+        return {
+            processing: false
+        };
+    }
+}
